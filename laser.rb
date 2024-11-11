5588ed3b9e528394d7cb9fb502b68f7196ef9e07
jmattaa <mattajonathan1@gmail.com>
2024-11-11 18:03:17 +0100

    update formula sha

--- laser.rb
+++ laser.rb
@@ -2,7 +2,7 @@ class Laser < Formula
   desc "another ls alternative"
   homepage "https://github.com/jmattaa/laser"
   url "https://github.com/jmattaa/laser/archive/v0.1.0.tar.gz"
-  sha256 "e269b9eeaec11f823524cd28c557b115b95a1fae"
+  sha256 "d4c320aaca0594b6cd066f7a05430e2f025f914517ac7799490e5b03a9bee281"
   version "0.1.0"
 
   def install
