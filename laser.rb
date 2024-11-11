10d4953836a267b5d9a33704f49f6d1216061293
jmattaa <mattajonathan1@gmail.com>
2024-11-11 17:57:49 +0100

    0.1.0

---
+++ laser.rb
@@ -0,0 +1,12 @@
+class laser < Formula
+  desc "another ls alternative"
+  homepage "https://github.com/jmattaa/laser"
+  url "https://github.com/jmattaa/laser/archive/v0.1.0.tar.gz"
+  sha256 "e269b9eeaec11f823524cd28c557b115b95a1fae"
+  version "0.1.0"
+
+  def install
+    system "make release"
+    bin.install "./bin/lsr"
+    system "make clean"
+  end
