d31c56dfda409d7db9401708b4c42bf34ad85204
jmattaa <mattajonathan1@gmail.com>
2024-11-20 23:40:20 +0100

    add shell completions

--- laser.rb
+++ laser.rb
@@ -1,13 +1,20 @@
 class Laser < Formula
   desc "another ls alternative"
   homepage "https://github.com/jmattaa/laser"
-  url "https://github.com/jmattaa/laser/archive/v0.2.1.tar.gz"
-  sha256 "c6892c1cd93a751681ed33b06f73fa6675c4340d2c037ab45f2d530f5774aa30"
-  version "v0.2.1"
+  url "https://github.com/jmattaa/laser/archive/v0.2.2.tar.gz"
+  sha256 "834402479c01e1cdf240902b75d620795e4153eab1e9c3c5b9d7bf06673195b9"
+  license "MIT"
+  version "v0.2.2"
+
+  depends_on "make" => :build
+  depends_on "gcc" => :build
 
   def install
     system "make release"
     bin.install "./bin/lsr"
+
+    generate_completions_from_executable(bin/"lsr", "--completions")
+
     system "make clean"
   end
 end
