5bf9c5ca9583f57b9a5a9cf193336e3355ad3af9
Jonathan Matta <mattajonathan1@gmail.com>
2024-12-12 23:06:31 +0100

    fix the installation to work actually

--- laser.rb
+++ laser.rb
@@ -10,10 +10,24 @@ class Laser < Formula
   depends_on "lua"
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DLUA_SCRIPTS_INSTALL_PATH=#{prefix}/lsr"
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
-    generate_completions_from_executable("build/lsr", "--completions")
+    bin.install "build/lsr"
+
+    generate_completions_from_executable(bin/"lsr", "--completions")
+  end
+
+  def post_install
+    home_dir = ENV["HOME"]
+    lsr_dir = "#{home_dir}/.lsr"
+
+    mkdir_p lsr_dir
+    cp_r Dir["#{prefix}/lsr/*"], lsr_dir
+  end
+
+  test do
+    system bin/"lsr", "--version"
   end
 end
